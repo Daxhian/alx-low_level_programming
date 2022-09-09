bcf115486f8f7cd767f9e6e76d77fe1b2631b9a7
Destiny Salami <daxhiian@gmail.com>
2022-09-08 20:35:33 -0700

    Intro to C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,16 @@
 */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	Printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	Printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	char charT;
+	int intT;
+	long int longT;
+	long long int longlongT;
+	float floatT;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(charT));
+	printf("Size of an int: %d byte(s)\n", sizeof(intT));
+	printf("Size of a long int: %d byte(s)\n", sizeof(longT));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(longlongT));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatT));
 	return (0);
 }
