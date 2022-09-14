c712da431617fe58cc6626a3b4cb51ea62ca69a4
Destiny Salami <daxhiian@gmail.com>
2022-09-14 12:37:10 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -10,6 +10,6 @@
 int _abs(int n)
 {
 	int absolute = abs(n);
-	
+
 	return (absolute);
 }
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_last_digit - Prints last didgit of the argument
+* @n: the argument (an integer) is accepted
+*
+* Return: the last digit of the integer
+*/
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	return (last_digit);
+}
