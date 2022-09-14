5b15b08291d683c6672d1b2c6338b2c8751e9b17
Destiny Salami <daxhiian@gmail.com>
2022-09-14 10:44:30 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,27 +3,27 @@
 
 /**
 * print_sign - checks the sign of an integer
-* @n - the interger value is received
+* @n: the interger value is received
 *
-* Retrun: Always 1 if n is positive
-* Always 0 if n is 0
-* Always -1 if n is negative
+* Return: Always 1 if n is positive
+* 		Always 0 if n is 0
+* 		Always -1 if n is negative
 */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		putchar('-');
+		_putchar('-');
 		return (-1);
 	}
 }
