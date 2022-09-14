220a24f30d69d1c7e3662e5d8bda641a246f01b6
Destiny Salami <daxhiian@gmail.com>
2022-09-14 12:31:16 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _abs - Prints the absolute value of an integer
+* @n: the value of the integer is accpeted
+*
+*  Return: ALways 0
+*/
+int _abs(int n)
+{
+	int absolute = abs(n);
+	return (absolute);
+}
