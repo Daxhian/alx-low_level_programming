1c4733dac8327bc9c62f53cfa138a2d1baeefe27
Destiny Salami <daxhiian@gmail.com>
2022-09-14 03:43:37 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <unistd.h>
 
 /**
 * main - Prints the char c to stdout
@@ -8,5 +8,5 @@
 */
 int _putchar(char c)
 {
-	return (c);
+	return (write(1, &c, 1));
 }
