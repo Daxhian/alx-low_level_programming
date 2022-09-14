18cda3285dfa0b7caee3ca3556ff868a6e5f2369
Destiny Salami <daxhiian@gmail.com>
2022-09-13 19:35:15 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 * main - Prints _putchar
 *
 * Return: 0
 */
-int _putchar(char c)
+int _putchar(void)
 {
 	printf("_putchar\n");
 	return (0);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-int _putchar(char c)
+int _putchar(void)
 {
 	return (0);
 }
