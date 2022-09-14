ad1084c81efa2c1f676647ce0486d5e006a013b3
Destiny Salami <daxhiian@gmail.com>
2022-09-14 13:03:55 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* times_table - Prints times table from 0 to 9
+*
+* Returns: Always 0
+*/
+void times_table(void)
+{
+	int m_ier;
+	int m_cand;
+	int product;
+
+	for (m_cand = 0; m_cand <= 9; m_cand++)
+	{
+		for (m_ier = 0; m_ier <= 9; m_ier++)
+		{
+			product = m_ier * m_cand;
+			if (m_ier < 9)
+			{
+				printf("%d, ", product);
+			}
+			else
+			{
+				printf("%d\n", product);
+			}
+		}
+	}
+	return (0);
+}
