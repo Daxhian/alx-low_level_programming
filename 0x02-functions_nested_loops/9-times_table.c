1293a361cef53524e63be382c9938e0fac05dd83
Destiny Salami <daxhiian@gmail.com>
2022-09-14 13:09:23 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,11 +19,11 @@ void times_table(void)
 			product = m_ier * m_cand;
 			if (m_ier < 9)
 			{
-				printf("%d, ", product);
+				putchar(product);
 			}
 			else
 			{
-				printf("%d\n", product);
+				_putchar(product);
 			}
 		}
 	}
