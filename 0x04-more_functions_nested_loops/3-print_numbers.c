302259fac82e62f0008e65cdb079e90052480caa
Destiny Salami <daxhiian@gmail.com>
2022-09-16 12:30:40 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_numbers - Prints 0 to 9 followed by a new line
+*
+* Return: void
+*/
+void print_numbers(void);
+{
+	int a, b;
+
+	if (a = '0'; a <= '9'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
