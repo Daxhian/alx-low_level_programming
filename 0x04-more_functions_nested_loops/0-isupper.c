a2a82fa511b8b44b77684cb46f85b50854c621ec
Destiny Salami <daxhiian@gmail.com>
2022-09-15 17:55:33 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+* _isupper - checks for uppercase character
+* @c: character is accepted
+*
+* Return: Always 1 if character uppercase but 0 if otherwise
+*/
+int _isupper(int c);
+{
+	int checker = isupper(c);
+
+	if  (checker = 1)
+	{
+		_putchar(c);
+		_putchar(':');
+		_putchar(' ');
+		_putchar(checker);
+	}
+	else
+	{
+		_putchar(c);
+		_putchar(':');
+		_putchar(' ');
+		_putchar(checker);
+	}
+}
