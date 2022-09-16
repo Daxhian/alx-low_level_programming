ef2149cbeb1dcb089c97a9740c502f80e0e7f6a2
Destiny Salami <daxhiian@gmail.com>
2022-09-16 12:57:38 -0700

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,11 +9,11 @@ void print_most_numbers(void)
 {
 	char a;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		if (a != '2' && a != '4')
+		if (a != 2 && a != 4)
 		{
-			putchar(a);
+			putchar(a + '0');
 		}
 	}
 	putchar('\n');
