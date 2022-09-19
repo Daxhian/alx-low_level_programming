199d2d18b37d3976932dfabeabda8a37cedd4418
Destiny Salami <daxhiian@gmail.com>
2022-09-19 17:21:51 +0100

    Intro to Pointers, Arrays and Strings

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,18 @@
+/**
+ * File: 1-swap.c
+ * Auth: Destiny Salami
+ */
 #include "main.h"
+
+/**
+ * 1-swap.c - Swaps the values of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+    *a = b;
+    *b = a;
+}
