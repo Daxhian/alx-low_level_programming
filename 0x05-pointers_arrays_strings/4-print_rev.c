e75d2cc81406841577f0a5ae17e6567367afa77a
Destiny Salami <daxhiian@gmail.com>
2022-09-19 10:56:52 -0700

    Intro to Pointers, Arrays and Strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,9 +10,9 @@
 void print_rev(char *s)
 {
 	int len = strlen(s); /* Variable to get the length of string */
-	int i;
+	int i = len -1;
 
-	for (i = len; i >= 0; i--)
+	for (i; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
