0e22975163060aec4917fa4eaa1d67d1c9d0bf95
Destiny Salami <daxhiian@gmail.com>
2022-09-19 06:39:09 -0700

    Intro to Pointers, Arrays and Strings

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef _main_h_
+#define _main_h_
+
+/**
+* File: main.h
+* Auth: Destiny Salami
+* Desc: Header file containing prototypes for all functions
+*       used in the 0x05-pointers_arrays_strings directory.
+*/
+
+int _putchar(char c);
+
+#endif
