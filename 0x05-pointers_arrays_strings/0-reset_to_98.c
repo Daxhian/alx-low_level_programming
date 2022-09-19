139d09a609f6dd1d17f373935b0488842cd9e673
Destiny Salami <daxhiian@gmail.com>
2022-09-19 17:07:52 +0100

    Intro to Pointers, Arrays and Strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,19 @@
+/**
+ * File: 0-reset_to_98.c
+ * Auth: Destiny Salami
+ */
 #include "main.h"
+
+/**
+ * reset_to_98 - Updates the value of the variable it points to
+ * @n: accepts variable
+ *
+ * Return: void
+ */
+void reset_to_98(int *n);
+{
+    int n;
+    n = &n;
+
+    *n = 98
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,12 +2,13 @@
 #define _main_h_
 
 /**
-* File: main.h
-* Auth: Destiny Salami
-* Desc: Header file containing prototypes for all functions
-*       used in the 0x05-pointers_arrays_strings directory.
-*/
+ * File: main.h
+ * Auth: Destiny Salami
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x05-pointers_arrays_strings directory.
+ */
 
 int _putchar(char c);
+void reset_to_98(int *n);
 
 #endif
