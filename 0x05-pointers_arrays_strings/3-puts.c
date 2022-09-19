559db4b0622ee15e485de036f9ec11b3c6b45a8b
Destiny Salami <daxhiian@gmail.com>
2022-09-19 10:37:12 -0700

    Intro to Pointers, Arrays and Strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _puts - prints a string, followed by a new line, to stdout
@@ -8,4 +9,12 @@
  */
 void _puts(char *str)
 {
+	int len = strlen(str); /* Variable get the length of string */
+	int i;
+
+	for (i = 0; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
