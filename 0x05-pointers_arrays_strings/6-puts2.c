fb7f4ea34681ad684c83d1bfae301c4cf593e210
Destiny Salami <daxhiian@gmail.com>
2022-09-19 18:57:42 -0700

    Intro to Pointers, Arrays and Strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,4 +9,16 @@
  */
 void puts2(char *str)
 {
+	int len = strlen(str);
+	int i;
+
+	for (i = 0; i < len; i++)
+	{
+		if (i % 2 != 0)
+		{
+			continue;
+		}
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
