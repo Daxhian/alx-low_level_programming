709d1e83aab7b1cbb4d79b99cfb2bb9b57fc18bf
Destiny Salami <daxhiian@gmail.com>
2022-09-21 10:12:53 -0700

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strcat - appends the source string to the destination string.
+* @dest - specifty destination string
+* @src - specify source string
+*
+* Return: dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	int len1 = strlen(src), len2 = strlen(dest);
+	int i;
+
+	for (i = 0; i < len1; i++)
+		dest[len2 + i] = src[i];
+	
+	return(dest);
+}
