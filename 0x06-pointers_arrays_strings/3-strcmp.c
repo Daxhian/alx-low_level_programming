b721f41f319a03328c1667d1bae0d3ecb9a96a2c
Destiny Salami <daxhiian@gmail.com>
2022-09-22 16:48:18 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
 * @s1: first string to be compared
 * @s2: second string to be compared
 *
-* Return: If str1 < str2, the negative difference of the first unmatched 
+* Return: If str1 < str2, the negative difference of the first unmatched
 *         characters
 *         If str1 == str2, 0
 *         If str1 > str2, the positive difference of the first unmatched
