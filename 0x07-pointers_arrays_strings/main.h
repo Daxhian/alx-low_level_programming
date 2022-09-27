8b0cd3257c87fdd30067e183996092f139dbc887
Destiny Salami <daxhiian@gmail.com>
2022-09-26 18:34:46 -0700

    pointers, arrays and strings

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef _main_h_
 #define _main_h_
 
+#include <stddef.h>
+
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
